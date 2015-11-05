64cef4e591d001d6359a79f8b3760659fbaadf81
Arial7 <riesinger.pascal@gmail.com>
2015-11-05 22:14:06 +0100

    Added many track symbols, cleaned up and commented JS

--- app/app.js
+++ app/app.js
@@ -5,52 +5,76 @@ var app = express();
 
 var projectRoot = __dirname;
 
+/**
+ * This function sets up the basic HTTP server to respond to requests
+ */
+function initializeServer() {
+    //set up the express server to use static dirs
+    app.use('/css', express.static(projectRoot + '/client/css'));
+    app.use('/js', express.static(projectRoot + '/client/js'));
+    app.use('/img', express.static(projectRoot + '/client/img'));
 
-//set up the express server to use static dirs
-app.use('/css', express.static(projectRoot + '/client/css'));
-app.use('/js', express.static(projectRoot + '/client/js'));
-app.use('/img', express.static(projectRoot + '/client/img'));
-
-//send the page over, if the user requests a connection
-app.get('/', function (req, res) {
-    res.sendFile('client/pages/main.html', {root : projectRoot});
-});
-
-//echo out the current port and address
-var server = app.listen(3030, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Listening at http://%s:%s', host, port);
-});
-//echo out all the serial ports that have been found
-serialPort.list(function(err, ports) {
-    ports.forEach(function(port) {
-        console.log("Port: " + port.comName);
-    });
-});
-//set up the io socket listeners
-io = io.listen(server);
-io.sockets.on('connection', function(socket) {
-    var msgToClient = "Connection with server established";
-    //Send the data to the client -> this will trigger the message event at the client
-    socket.send(msgToClient);
-    console.log('Socket.io : Connection with client established');
-
-    socket.on('message', function(data) {
-        data = JSON.parse(data);
-        console.log(data);
-        var ackToClient = "Server received message";
-        //Send back an acknowledgement to the client
-        socket.send(ackToClient);
+    //send the page over, if the user requests a connection
+    app.get('/', function (req, res) {
+        res.sendFile('client/pages/main.html', {root : projectRoot});
     });
 
-    socket.on('toggle message', function(data) {
-        console.log("Socket.io : received toggle message: " + data);
-        socket.send("Toggled: " + data);
-    });
+    //echo out the current port and address
+    var server = app.listen(3030, function () {
+        var host = server.address().address;
+        var port = server.address().port;
 
-    socket.on('disconnect', function(socket) {
-        console.log('Socket.io : Client disconnected');
+        console.log('Listening at http://%s:%s', host, port);
     });
-});
+}
+/**
+ * This function sets up the socket.io listeners for connecting, disconnecting etc.
+ * TODO: log to the console with timestamps
+ */
+function setupSocketListeners() {
+    //set up the io socket listeners
+    io = io.listen(server);
+    io.sockets.on('connection', function(socket) {
+        var msgToClient = "Connection with server established";
+        //Send the data to the client -> this will trigger the message event at the client
+        socket.send(msgToClient);
+        console.log('Socket.io : Connection with client established');
+
+        socket.on('message', function(data) {
+            data = JSON.parse(data);
+            console.log(data);
+            var ackToClient = "Server received message";
+            //Send back an acknowledgement to the client
+            socket.send(ackToClient);
+        });
+
+        socket.on('toggle message', function(data) {
+            console.log("Socket.io : received toggle message: " + data);
+            socket.send("Toggled: " + data);
+        });
+
+        socket.on('disconnect', function(socket) {
+            console.log('Socket.io : Client disconnected');
+        });
+    });
+}
+/**
+ * This function scans for avaiable serial ports and logs them to the console
+ * TODO: save the found ports somewhere
+ * TODO: refresh the ports from time to time
+ */
+function scanSerialPorts() {
+    //echo out all the serial ports that have been found
+    serialPort.list(function(err, ports) {
+        if (err !== undefined) {
+            ports.forEach(function(port) {
+                console.log("Port: " + port.comName);
+            });
+        }
+    });
+}
+
+initializeServer();
+setupSocketListeners();
+
+scanSerialPorts();
--- app/client/js/client.js
+++ app/client/js/client.js
@@ -1,54 +1,118 @@
 //TODO: a lot of cleanup
 
-var socket = io();
-socket.on('message', function(data) {
-    var message = data;
-    console.log('Message from server: ' + message);
-    logToConsole(message);
-});
+(function($){
 
-$(function(){
-    //TODO: temp: generate a lot of toggle buttons
-    for(var x = 0; x < 23; x++) {
-        for(var y = 0; y < 10; y++) {
-            $('.controls').append('<div class="toggle-btn" data-id="' + x * (y + x) + '" data-icn="WL0-R", data-x="' + x + '" data-y="' + y + '"></div>');
+    var $console = $('.console'),
+        $consoleMessages = $('.console .messages'),
+        $controls = $('.controls'),
+        $activitySpinner = $('.spinner.activity'),
+        socket = io();
+
+
+    //ENTRY POINT
+    $(function() {
+        initClient();
+    });
+
+    /**
+     * Loads the current plan of tracks from the server and parses it
+     * TODO: outsource this method to another file
+     */
+    function loadTracks() {
+        //TODO: temp: generate a lot of toggle buttons
+        for(var x = 0; x < 23; x++) {
+            for(var y = 0; y < 8; y++) {
+                //TODO: create skeletons for the toggle buttons and track icons
+                $controls.append('<div class="track switch" data-id="' + (x + y + 1) * (y + x + 1) + '" data-icn="g0" data-x="' + x + '" data-y="' + y + '"></div>');
+            }
+        }
+
+        //Set up the toggle buttons
+        $('.track.switch').click(function(){
+            socket.emit('toggle message', $(this).attr('data-id'));
+        });
+
+        $('.track.switch').each(function() {
+            $(this).addClass($(this).attr('data-icn'));
+        });
+
+        alignControls();
+
+    }
+
+    /**
+     * Sets up all of the socket, click and general event handlers
+     */
+    function setupListeners() {
+        socket.on('message', function(data) {
+            var message = data;
+            console.log('Message from server: ' + message);
+            logToConsole(message);
+        });
+
+        $console.find('.toggle').on('click', function() {
+            $console.toggleClass('collapsed');
+            $controls.toggleClass('console-collapsed');
+
+            var collapsed = $console.hasClass('collapsed').toString();
+            Cookies.set('collapsedConsole', collapsed, {expires: 999999999});
+
+        });
+    }
+
+    /**
+     * Initializes the client core
+     */
+    function initClient() {
+        setupUI();
+        setupListeners();
+        loadTracks();
+        $activitySpinner.addClass('hidden');
+        console.log('Client initialized');
+    }
+
+    /**
+     * Handles initialization of some UI components
+     */
+    function setupUI() {
+        if (Cookies.get('collapsedConsole') == "true") {
+            $console.addClass('collapsed');
+            $controls.addClass('console-collapsed');
         }
     }
 
-    //Set up the toggle buttons
-    $('.toggle-btn').click(function(){
-        socket.emit('toggle message', $(this).attr('data-id'));
-    });
+    /**
+     * Wrapper for adding objects or plain text to the console.
+     * This adds a timestamp in front of the actual object or text
+     */
+    function logToConsole(data) {
+        //append the current message (or object)
+        $('.console .messages').append('<div>[' + getCurrentTime() + '] ' + data + '</div>');
+        //calculate the height used by the objects and scroll to the bottom
+        var h = 0;
+        $('.console .messages').children().each(function(){ h += parseInt($(this).height());});
+        h += '';
+        $('.console .messages').animate({scrollTop: h}, 100);
+    }
 
-    $('.toggle-btn').css('background-image: ', 'url("/img/' + $(this).attr('data-icn') + '.png")');
+    /**
+     * Function to get the current timestamp
+     * @return String of HOURS:MINUTES:SECONDS
+     */
+    function getCurrentTime() {
+        var date = new Date();
+        return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    }
 
-    alignControls();
-
-    $('.console .toggle').on('click', function() {
-        $('.console').toggleClass('collapsed');
-        $('.controls').toggleClass('console-collapsed');
-    });
-
-    console.log('Client initialized');
-});
-
-function logToConsole(data) {
-    $('.console .messages').append('<div>[' + getCurrentTime() + '] ' + data + '</div>');
-    var h = 0;
-    $('.console .messages').children().each(function(){ h += parseInt($(this).height());});
-    h += '';
-    $('.console .messages').animate({scrollTop: h}, 100);
-}
-
-function getCurrentTime() {
-    var date = new Date();
-    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(); 
-}
-
-function alignControls() {
-    $('.controls').children().each(function() {
-        //TODO: get the values from somewhere, exp the width of the icons
-        $(this).css('left', (50 * parseInt($(this).attr('data-x')) + 40) + 'px');
-        $(this).css('top', (50 * parseInt($(this).attr('data-y')) + 40) + 'px');
-    });
-}
+    /**
+     * This puts all of the toggle buttons and track icons to their intended
+     * position based on their data-x and data-y value (these represent the grid)
+     */
+    function alignControls() {
+        $('.controls').children().each(function() {
+            //TODO: get the values from somewhere, exp the width of the icons
+            $(this).css('left', (50 * parseInt($(this).attr('data-x')) + 40) + 'px');
+            $(this).css('top', (50 * parseInt($(this).attr('data-y')) + 40) + 'px');
+        });
+    }
+})(jQuery)
